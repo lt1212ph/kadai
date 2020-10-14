ececbcef422c2ffefd4c43c48546702236192ccb
lt1212ph <lt1212ph@gmail.com>
2020-10-14 05:27:21 +0000

    第2回課題

---
+++ kadai-02/bool_description.js
@@ -0,0 +1,15 @@
+// 数字 n が 偶数である
+// 数字 n　が 3の倍数である
+
+const n = 9
+const isGusu = false
+const is3x = true
+
+// 数字 n が 偶数である かつ 3の倍数である => AND 演算
+isGusu && is3x // false && true === false
+
+// 数字 n が偶数である または 3の倍数である　=> OR 演算
+isGusu || is3x // false || true === true
+
+// 数字 n が 偶数でない => NOT 演算
+!isGusu // !false === true
---
+++ kadai-02/index.js
@@ -0,0 +1,18 @@
+// 文字列を結合して表示する
+const firstName = 'lt'
+const familyName = '1212ph'
+
+console.log(firstName + familyName)
+
+// 1から10までの和を表示する
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+// 真偽型の演算
+const isDog = false
+const isCat = false
+// 犬または猫を飼っているかどうか
+console.log(isDog || isCat)
+// 犬と猫の両方を飼っているかどうか
+console.log(isDog && isCat)
+// 犬と猫両方とも飼っていないかどうか
+console.log(!isDog && !isCat)
