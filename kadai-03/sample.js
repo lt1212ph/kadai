bd8978e9b7a2cd1318550b6cd91a0cde7390295c
lt1212ph <lt1212ph@gmail.com>
2020-10-27 14:04:30 +0000

    第3回課題

---
+++ kadai-03/sample.js
@@ -0,0 +1,12 @@
+const score = 75
+if (score > 90) {
+    console.log('S')
+} else if (score >= 80) {
+    console.log('A')
+} else if (score >= 70) {
+    console.log('B')
+} else if (score >= 60) {
+    console.log('C')
+} else {
+    console.log('F')
+}
