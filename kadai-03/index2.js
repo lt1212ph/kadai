91b621eb40f2b3392021f22e8b85b111b11d286b
lt1212ph <lt1212ph@gmail.com>
2020-10-28 05:21:45 +0000

    第3回課題

--- kadai-03/index2.js
+++ kadai-03/index2.js
@@ -1,9 +1,13 @@
 // 最⼤の数を⼊⼒する
-const max= parseInt (process.argv[2])
+const max = parseInt (process.argv[2])
 for (let n = 1; n < max; n = n + 1) {
 if (n % 10 === 3) {
 console.log ('3がつく数字です')
-} else {
-console.log ()
+} else if (n < 30) {
+console.log (n)
+} else if (n < 40) {
+    console.log ('3がつく数字です')
+} else if(40 <= n) {
+    console.log (n)
 }
 }
