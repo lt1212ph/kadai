89773a6906543cd0513b21e612de66cbe61de757
lt1212ph <lt1212ph@gmail.com>
2020-10-28 04:07:05 +0000

    第3回課題

--- kadai-03/index1.js
+++ kadai-03/index1.js
@@ -1,9 +1,9 @@
 // 生まれた日を入力
 const day = parseInt(process.argv[2])
 if (day % 3 === 0) {
-    console.log('あなたは○○な性格です')
+    console.log('あなたはいい人です')
 } else if (day % 3 === 1) {
-    console.log('あなたは△△な性格です')
+    console.log('あなたは悪くない人です')
 } else {
-    console.log('あなたは**な性格です')
+    console.log('あなたは素晴らしい人です')
 }
