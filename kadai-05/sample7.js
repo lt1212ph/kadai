d8cf60f9e1bed48dbbc9e921b853a4f530701d85
lt1212ph <lt1212ph@gmail.com>
2020-11-11 05:29:11 +0000

    第5回サンプル

---
+++ kadai-05/sample7.js
@@ -0,0 +1,5 @@
+const animals = ['dog','cat','rabbit','bird','fish','beetle']
+for (let index = 0; index < animals.length; index = index + 1) {
+    const animal = animals[index];
+    console.log(animal)
+}
