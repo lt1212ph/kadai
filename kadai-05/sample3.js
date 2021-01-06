6f0fa4b1d1e5c453a90273eb2cd27d33758ccb70
lt1212ph <lt1212ph@gmail.com>
2021-01-06 06:01:57 +0000

    第5回課題

--- kadai-05/sample3.js
+++ kadai-05/sample3.js
@@ -6,3 +6,5 @@ const person = {
 console.log(person.age)
 person.age = 20 //オブジェクトのプロパティ値に代入することができる
 console.log(person.age)
+person['age'] = 20 //もちろんブラケット記法でもOK
+console.log(person.age)
--- kadai-05/sample8.js
+++ kadai-05/sample8.js
@@ -1,3 +1,18 @@
-const(person) = {
-    
+const person = {
+    name: 'arai',
+    age: 21,
+    'has-cat': false
 }
+// 組み込みのオブジェクトである Object を使う
+// Object には keys(), values() のようなメソッドがある
+
+const keys = Object.keys(person)
+console.log(keys)
+const values =Object.values(person)
+console.log(values)
+
+const prefectures = {
+    '01': '北海道',
+    '02': '青森県'
+}
+const prefCodes = Object.keys(prefectures) // ['01', '02', ...]
